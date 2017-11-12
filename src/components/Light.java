c280da8c3a772a51c5d32f6bae6ddf11937f711b
Gabriel Urbaits <gurbaitis@unm.edu>
2017-11-12 16:03:22 -0700

    Added Light Functionality

--- src/components/Light.java
+++ src/components/Light.java
@@ -2,11 +2,33 @@ package components;
 
 public class Light extends Component
 {
-
-
+    private boolean on = false;
     @Override
     void update()
     {
-
+        justWait();
+        System.out.println(getName() + " received message and woke up." + " (" + isLock() + ")");
+        if(getLeft().isLock()||getRight().isLock()){
+            setOn(true);
+        }
+        if(!getLeft().isLock()&&!getRight().isLock()){
+            setOn(false);
+        }
+        processMessage();
     }
+
+    private void processMessage()
+    {
+        Component neighbor = getNeighbor(getMsg().getDirection());
+        neighbor.accept(getMsg());
+    }
+    public boolean isOn()
+    {
+        return on;
+    }
+    public void setOn(boolean on){
+        this.on = on;
+        System.out.println("on = "+on);
+    }
+
 }
