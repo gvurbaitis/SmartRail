9913ea74caf1b1730ae48e08d8525853d33cb05b
Gabriel Urbaits <gurbaitis@unm.edu>
2017-11-16 13:42:59 -0700

    Cleaned up Light and Station.

--- src/main/components/Component.java
+++ src/main/components/Component.java
@@ -18,241 +18,262 @@ package main.components;
 //***********************************
 public abstract class Component implements Runnable
 {
-    private Message msg;
-    private String name;
-    private String group;
+  private Message msg;
+  private String name;
+  private String group;
 
-    private Component right;
-    private Component left;
+  private Component right;
+  private Component left;
 
-    private boolean shutdown;
-    private boolean lock;
+  private boolean shutdown;
+  private boolean lock;
 
-    private double x, y;
+  private double x, y;
 
 
-    /**
-     * Input: None
-     * Returns void
-     * Gathers the name and group from the thread
-     * for each component and runs the associated
-     * update() method until a shutdown boolean is
-     * true.
-     */
-    @Override
-    public void run()
+  /**
+   * Input: None
+   * Returns void
+   * Gathers the name and group from the thread
+   * for each component and runs the associated
+   * update() method until a shutdown boolean is
+   * true.
+   */
+  @Override
+  public void run()
+  {
+    shutdown = false;
+    name = Thread.currentThread().getName();
+    group = Thread.currentThread().getThreadGroup().getName();
+
+    while (!shutdown)
     {
-        shutdown = false;
-        name = Thread.currentThread().getName();
-        group = Thread.currentThread().getThreadGroup().getName();
-
-        while (!shutdown)
-        {
-            update();
-        }
+      update();
     }
+  }
 
-    /**
-     * Input: None
-     * Returns void
-     * Performs the actions unique to
-     * each component while its thread
-     * is alive.
-     */
-    abstract void update();
+  /**
+   * Input: None
+   * Returns void
+   * Performs the actions unique to
+   * each component while its thread
+   * is alive.
+   */
+  abstract void update();
 
-    /**
-     * Input: Message msg: Passes information
-     * such as the direction of transfer and the
-     * station of departure and destination.
-     * Returns void
-     * Sets the msg field of the component to the
-     * message passed, and notifies the thread out
-     * of a waiting state.
-     */
-    synchronized void accept(Message msg)
+  /**
+   * Input: Message msg: Passes information
+   * such as the direction of transfer and the
+   * station of departure and destination.
+   * Returns void
+   * Sets the msg field of the component to the
+   * message passed, and notifies the thread out
+   * of a waiting state.
+   */
+  synchronized void accept(Message msg)
+  {
+    this.msg = msg;
+    notify();
+  }
+
+  /**
+   * Input: None
+   * Returns void
+   * Tries to put the thread into a wait,
+   * catches an interrupted exception and
+   * prints the stack trace if applicable.
+   */
+  synchronized void justWait()
+  {
+    try
     {
-        this.msg = msg;
-        notify();
-    }
-
-    /**
-     * Input: None
-     * Returns void
-     * Tries to put the thread into a wait,
-     * catches an interrupted exception and
-     * prints the stack trace if applicable.
-     */
-    synchronized void justWait()
+      wait();
+    } catch (InterruptedException e)
     {
-        try
-        {
-            wait();
-        }
-        catch (InterruptedException e)
-        {
-            e.printStackTrace();
-        }
+      e.printStackTrace();
     }
+  }
 
-    /**
-     * Input: int direction, 1 = right,
-     * -1 = left
-     * Returns Component, either the neighbor
-     * on the right or on the left of the
-     * component
-     * returns the Neighbor on the right of the
-     * component if the direction is 1, otherwise
-     * returns the left neighbor
-     */
-    Component getNeighbor(int direction)
-    {
-        if (direction == 1) return right;
-        else return left;
-    }
+  /**
+   * Input: int direction, 1 = right,
+   * -1 = left
+   * Returns Component, either the neighbor
+   * on the right or on the left of the
+   * component
+   * returns the Neighbor on the right of the
+   * component if the direction is 1, otherwise
+   * returns the left neighbor
+   */
+  Component getNeighbor(int direction)
+  {
+    if (direction == 1) return right;
+    else return left;
+  }
 
-    /**
-     * Input: none
-     * Returns String name, the name of the thread
-     * the component is using
-     * returns the name of the thread
-     * the component is using
-     */
-    public String getName()
-    {
-        return this.name;
-    }
+  /**
+   * Input: none
+   * Returns String name, the name of the thread
+   * the component is using
+   * returns the name of the thread
+   * the component is using
+   */
+  public String getName()
+  {
+    return this.name;
+  }
 
-    /**
-     * Input: none
-     * Returns String group, the group of the thread
-     * the component is using
-     * returns the group of the thread
-     * the component is using
-     */
-    public String getGroup()
-    {
-        return this.group;
-    }
+  /**
+   * Input: none
+   * Returns String group, the group of the thread
+   * the component is using
+   * returns the group of the thread
+   * the component is using
+   */
+  public String getGroup()
+  {
+    return this.group;
+  }
 
-    /**
-     * Input: none
-     * Returns Message msg, the last message passed
-     * to the component
-     * returns the last message the
-     * component received
-     */
-    Message getMsg()
-    {
-        return msg;
-    }
+  /**
+   * Input: none
+   * Returns Message msg, the last message passed
+   * to the component
+   * returns the last message the
+   * component received
+   */
+  Message getMsg()
+  {
+    return msg;
+  }
 
-    /**
-     * Input: none
-     * Returns void
-     * sets the shutdown boolean to true,
-     * ending the while loop running the
-     * thread.
-     */
-    void shutdown()
-    {
-        this.shutdown = true;
-    }
+  /**
+   * Input: none
+   * Returns void
+   * sets the shutdown boolean to true,
+   * ending the while loop running the
+   * thread.
+   */
+  void shutdown()
+  {
+    this.shutdown = true;
+  }
 
-    /**
-     * Input: none
-     * Returns boolean shutdown, whether the
-     * thread is alive
-     * Checks whether the thread has been shutdown
-     */
-    public boolean isShutdown()
-    {
-        return this.shutdown;
-    }
+  /**
+   * Input: none
+   * Returns boolean shutdown, whether the
+   * thread is alive
+   * Checks whether the thread has been shutdown
+   */
+  public boolean isShutdown()
+  {
+    return this.shutdown;
+  }
 
-    /**
-     * Input: Component Right
-     * Returns void
-     * sets the inputted component to the
-     * right field of the current component
-     */
-    public void setRight(Component right)
-    {
-        this.right = right;
-    }
+  /**
+   * Input: Component Right
+   * Returns void
+   * sets the inputted component to the
+   * right field of the current component
+   */
+  public void setRight(Component right)
+  {
+    this.right = right;
+  }
 
-    /**
-     * Input: Component Left
-     * Returns void
-     * sets the inputted component to the
-     * left field of the current component
-     */
-    public void setLeft(Component left)
-    {
-        this.left = left;
-    }
+  /**
+   * Input: none
+   * Returns Component left, the component
+   * to the left of the current component
+   * returns  the component to the left
+   * of the current component
+   */
+  public Component getLeft()
+  {
+    return left;
+  }
 
-    /**
-     * Input: none
-     * Returns Component left, the component
-     * to the left of the current component
-     * returns  the component to the left
-     * of the current component
-     */
-    public Component getLeft()
-    {
-        return left;
-    }
+  /**
+   * Input: Component Left
+   * Returns void
+   * sets the inputted component to the
+   * left field of the current component
+   */
+  public void setLeft(Component left)
+  {
+    this.left = left;
+  }
 
-    /**
-     * Input: none
-     * Returns double x, the horizontal
-     * position of the component
-     * returns the horizontal
-     * position of the component in the GUI
-     */
-    public double getX() { return x; }
+  /**
+   * Input: none
+   * Returns double x, the horizontal
+   * position of the component
+   * returns the horizontal
+   * position of the component in the GUI
+   */
+  public double getX()
+  {
+    return x;
+  }
 
-    /**
-     * Input: double x, the horizontal position of the component
-     * Returns void sets the horizontal
-     * position of the component in the GUI
-     */
-    public void setX(double x) { this.x = x; }
+  /**
+   * Input: double x, the horizontal position of the component
+   * Returns void sets the horizontal
+   * position of the component in the GUI
+   */
+  public void setX(double x)
+  {
+    this.x = x;
+  }
 
-    /**
-     * Input: none
-     * Returns double y, the vertical position of the component
-     * returns the vertical position of the component in the GUI
-     */
-    public double getY() { return y; }
+  /**
+   * Input: none
+   * Returns double y, the vertical position of the component
+   * returns the vertical position of the component in the GUI
+   */
+  public double getY()
+  {
+    return y;
+  }
 
-    /**
-     * Input: double y, the vertical position of the component
-     * Returns void sets the vertical position
-     * of the component in the GUI
-     */
-    public void setY(double y) { this.y = y; }
+  /**
+   * Input: double y, the vertical position of the component
+   * Returns void sets the vertical position
+   * of the component in the GUI
+   */
+  public void setY(double y)
+  {
+    this.y = y;
+  }
 
-    /**
-     * Input: none
-     * Returns boolean lock, the
-     * protection of the component returns whether or
-     * not the component can be traversed
-     */
-    boolean isLock() { return lock; }
+  /**
+   * Input: none
+   * Returns boolean lock, the
+   * protection of the component
+   * returns whether or not the
+   * component can be traversed
+   */
+  boolean isLock()
+  {
+    return lock;
+  }
 
-    /**
-     * Input: none
-     * Returns void
-     * sets lock to false, indicating the component may not be traversed
-     */
-    void unlock() { this.lock = false; }
+  /**
+   * Input: none
+   * Returns void
+   * sets lock to false, indicating the component may not be traversed
+   */
+  void unlock()
+  {
+    this.lock = false;
+  }
 
-    /**
-     * Input: none
-     * Returns void
-     * sets lock to true, indicating the component may be traversed
-     */
-    void lock() { this.lock = true; }
+  /**
+   * Input: none
+   * Returns void
+   * sets lock to true, indicating the component may be traversed
+   */
+  void lock()
+  {
+    this.lock = true;
+  }
 }
--- src/main/components/Station.java
+++ src/main/components/Station.java
@@ -1,48 +1,132 @@
 package main.components;
-
+//***********************************
+// Gabriel Urbaitis, Akash Patel
+//
+// The Light Class performs much like
+// a track with similar message passing,
+// however it has an on field which is
+// invoked when a message has found a
+// secured path and is on its way back
+// to the Train. It may also be set back
+// to red with the public SetOn method
+// to allow the train to alter its state
+// when moving past it.
+//***********************************
 public class Station extends Component
 {
-    private Track originator; // the initial track connected to the station
-    private int type; // 0 == normal station, 1 == dead end
+  private Track originator; // the initial track connected to the station
+  private int type; // 0 == normal station, 1 == dead end
+  /**
+   * Input: None
+   * Returns void
+   * As the update method is
+   * within a while loop, the
+   * thread waits until a message is
+   * passed so as not to use resources.
+   * Accepting a message notifies the
+   * thread and allows the Station to
+   * send a confirmation, if
+   * it is the correct station, before
+   * going into another wait.
+   */
+  void update()
+  {
+    justWait();
 
-    void update()
+    // if true then the correct station is found so,
+    // send confirmation to the train
+    if (processMessage()) sendConfirmation();
+  }
+  /**
+   * Input: None
+   * Returns void
+   * Process Message is fairly simple if
+   * the destination hasn't been found yet, or
+   * if the path was found to be invalid,
+   * it simply passes it on to the next neighbor.
+   * If the message is returning to the station
+   * the message is passed, but the component is
+   * also locked, and the light is turned on.
+   */
+  private boolean processMessage()
+  {
+    // only process message if not a dead end
+    if (type == 0)
     {
-        justWait();
+      String destination = getMsg().getDestination();
 
-        // if true then the correct station is found so, send confirmation to the train
-        if (processMessage()) sendConfirmation();
+      if (getName().equals(destination))
+      {
+        getMsg().setValidPath(true);
+
+        return true;
+      }
     }
 
-    private boolean processMessage()
-    {
-        // only process message if not a dead end
-        if (type == 0)
-        {
-            String destination = getMsg().getDestination();
-
-            if (getName().equals(destination))
-            {
-                getMsg().setValidPath(true);
-
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private void sendConfirmation()
-    {
-        getMsg().setDestination(getMsg().getTrainName());
-        getMsg().setDirection(getMsg().getDirection() * -1);
-        originator.accept(getMsg());
-    }
-
-    public void setOriginator(Track originator) { this.originator = originator; }
-
-    public Track getOriginator() { return originator; }
-
-    public int getType() { return type; }
-
-    public void setType(int type) { this.type = type; }
+    return false;
+  }
+  /**
+   * Input: None
+   * Returns void
+   * Being called upon finding the
+   * correct station, send confirmation
+   * alters the passed message to set
+   * the train as the new destination
+   * and reverse the direction before
+   * passing it back to the originator.
+   */
+  private void sendConfirmation()
+  {
+    getMsg().setDestination(getMsg().getTrainName());
+    getMsg().setDirection(getMsg().getDirection() * -1);
+    originator.accept(getMsg());
+  }
+  /**
+   * Input: none
+   * Returns Track originator, the
+   * track the station is connected to.
+   * Gets the track connected to the
+   * current station
+   */
+  public Track getOriginator()
+  {
+    return originator;
+  }
+  /**
+   * Input: Track originator, the
+   * track the station is to be
+   * connected to.
+   * Returns void
+   * Sets the track connected to the
+   * current station to the inputted
+   * track
+   */
+  public void setOriginator(Track originator)
+  {
+    this.originator = originator;
+  }
+  /**
+   * Input: none
+   * Returns void int type, 0 for a normal
+   * station, 1 for a dead end
+   * Gets the type of the station
+   * to process whether it is
+   * a dead end or normal station
+   */
+  public int getType()
+  {
+    return type;
+  }
+  /**
+   * Input: int type, 0 for a normal
+   * station, 1 for a dead end
+   * Returns void
+   * Sets the type of the station
+   * to indicate whether it is
+   * a dead end or normal station
+   */
+  public void setType(int type)
+  {
+    this.type = type;
+  }
 }
